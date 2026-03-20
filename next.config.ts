eeeb04270d7c8179d5c5388627fff72ba100df04
petritrexha <petrit.rexha@umib.net>
2026-03-21 00:31:33 +0100

    added some react changes version to fix error when opening albums

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from 'next'
-import path from 'path'
 
 const nextConfig: NextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      // Force a single copy of React throughout the entire bundle.
-      // This fixes the "Cannot read properties of undefined (reading
-      // 'ReactCurrentBatchConfig')" error caused by react-konva /
-      // react-reconciler bundling their own React copy.
-      'react': path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-    }
-    return config
   },
 }
 
